ed6c860f80f92d5900915d1f464ed11527205c48
sakurakiki89 <mariansimao@gmail.com>
2026-06-24 11:30:58 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Mariana and is {""}
-        <a
-          href="https://github.com/marianasimao89/react-weather-app"
-          target="_blank"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Mariana and is {""}
+          <a
+            href="https://github.com/marianasimao89/react-weather-app"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
