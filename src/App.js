3083526e2009b40764c2512da0bd6013d4311b20
sakurakiki89 <mariansimao@gmail.com>
2026-06-24 10:51:43 +0200

    added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Mariana and is {""}
+        <a
+          href="https://github.com/marianasimao89/react-weather-app"
+          target="_blank"
+        >
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
