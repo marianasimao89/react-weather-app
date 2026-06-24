8d47c8e35e181675c6ed3e21ffbcdfcb0cc4fb9c
sakurakiki89 <mariansimao@gmail.com>
2026-06-24 14:51:44 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Weather from "./Weather";
+import "./Weather.css";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Mariana and is {""}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00 </li>
+        <li> Sunny</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg"
+            alt="Sunny"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li> Humidity: 69%</li>
+            <li>Wind: 11km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
